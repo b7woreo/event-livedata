f726887a2aedbd3f6a493a18be1a9a03c1e8f2c1
chrnie <chrnie@qq.com>
2019-05-26 21:24:07 +0800

    Migrate androidTest to test

--- library/src/test/java/com/chrnie/live/data/TestLifecycleOwner.java
+++ library/src/test/java/com/chrnie/live/data/TestLifecycleOwner.java
@@ -2,16 +2,21 @@ package com.chrnie.live.data;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleRegistry;
+import androidx.lifecycle.Lifecycle.State;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LifecycleRegistry;
 
 public class TestLifecycleOwner implements LifecycleOwner {
 
-  public final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
+  private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
 
   @NonNull
   @Override
   public Lifecycle getLifecycle() {
     return lifecycleRegistry;
+  }
+
+  public void markState(@NonNull State state) {
+    lifecycleRegistry.markState(state);
   }
 }
